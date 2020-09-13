28f1d546393d52b2c6558707d031a5f334f913fb
Thiago de Oliveira Alves <towo497@gmail.com>
2020-09-13 01:29:03 -0300

    criação da classe Texto, refetoração de CalculadoraDeSemelhança

--- CalculadoraDeSemelhança.java
+++ CalculadoraDeSemelhança.java
@@ -7,68 +7,30 @@ import java.util.TreeSet;
 
 public class CalculadoraDeSemelhança {
 	
-	private String texto1; // texto 1
-	private String texto2; // texto 2
-	private String fonte1; // fonte do texto 1
-	private String fonte2; // fonte do texto 2
-	private String lowTexto1; // texto 1 com todas as letras minúsculas
-	private String lowTexto2; // texto 2 com todas as letras minúsculas
-	private String maxSubseq;
-	private String formatTexto1; // texto 1 com a maior subsequência comum marcada
-	private String formatTexto2; // texto 2 com a maior subsequência comum marcada
-	private StringTokenizer stk1; // tokenizer do texto1
-	private StringTokenizer stk2; // tokenizer do texto2
-	private StringTokenizer lowStk1; // tokenizer do lowTexto1
-	private StringTokenizer lowStk2; // tokenizer do lowWexto2
+	private Texto texto1; // texto 1
+	private Texto texto2; // texto 2
 	private StringTokenizer stkMaxSubseq; // tokenizer da maior subsequência comum
-	private Set<String> conjTexto1; // conjunto de palavras diferentes do texto 1
-	private Set<String> conjTexto2; // conjunto de palavras diferentes do texto 2
 	private Set<String> conjTextoComum; // conjunto de palavras diferentes comuns aos dois textos
 	private int totalWords;	// total de palavras diferentes usadas nos dois textos
+	private String maxSubseq; // maior subsequência comum aos dois textos
 	
 	public CalculadoraDeSemelhança(String fonte1, String texto1, String fonte2, String texto2) {
 		// configura as fontes e os textos
-		this.fonte1 = fonte1;
-		this.fonte2 = fonte2;
-		this.texto1 = texto1;
-		this.texto2 = texto2;
+		this.texto1 = new Texto(fonte1, texto1);
+		this.texto2 = new Texto(fonte2, texto2);
 		
-		// armazena os textos em letras minúsculas
-		lowTexto1 = texto1.toLowerCase();
-		lowTexto2 = texto2.toLowerCase();
-		
-		// separa as palavras dos textos para futura contagem
-		stk1 = new StringTokenizer(texto1, " (){}[]\"\',.;:") ;
-		stk2 = new StringTokenizer(texto2, " (){}[]\",.;:");
-		
-		// separa as palavras dos textos com minúscula para agrupamento
-		lowStk1 = new StringTokenizer(lowTexto1, " (){}[]\"\',.;:");
-		lowStk2 = new StringTokenizer(lowTexto2, " (){}[]\"\',.;:");
-		
-		// cria conjuntos para colocar as palavras com letras minúsculas
-		conjTexto1 = new TreeSet<>();
-		conjTexto2 = new TreeSet<>();
-		
-		// coloca as palavras de cada tokenizer em seus respectivos conjuntos
-		// eliminando assim as duplicatas
-		while (lowStk1.hasMoreTokens()) {
-			conjTexto1.add(lowStk1.nextToken());
-		}
-
-		while (lowStk2.hasMoreTokens()) {
-			conjTexto2.add(lowStk2.nextToken());
-		}
-
 		// cria um mapa para contar as palavras
 		HashMap<String, Integer> AllWords = new HashMap<String, Integer>();
 
 		// adociona as palavras do primeiro conjunto ao mapa
-		for (String word : conjTexto1) {
+		for (int i = 0; i < this.texto1.getConjTextoSize(); i++) {
+			String word = this.texto1.getConjTexto(i);
 			AllWords.put(word, 1);
 		}
 
 		// adiciona as palavras do segundo conjunto ao mapa incrementando as repetidas
-		for (String word : conjTexto2) {
+		for (int i = 0; i < this.texto2.getConjTextoSize(); i++) {
+			String word = this.texto2.getConjTexto(i);
 			if (!AllWords.containsKey(word)) {
 				AllWords.put(word, 1);
 			} else {
@@ -113,8 +75,8 @@ public class CalculadoraDeSemelhança {
 		maxSubseq = (String) texto1.substring(x + 1 - maxLen, x + 1); // ou maxSubseq = (String) texto2.substring(j + 1 - maxLen, j + 1);
 		
 		// marca a maior subsequência comum nos dois textos
-		formatTexto1 = texto1.replace(maxSubseq, maxSubseq.toUpperCase());
-		formatTexto2 = texto2.replace(maxSubseq, maxSubseq.toUpperCase());
+		this.texto1.setFormatTexto(texto1.replace(maxSubseq, maxSubseq.toUpperCase()));
+		this.texto2.setFormatTexto(texto2.replace(maxSubseq, maxSubseq.toUpperCase()));
 		
 		// separ as palavras do maior trecho comum
 		stkMaxSubseq = new StringTokenizer(maxSubseq, " (){}[]\"\',.;:");
@@ -138,44 +100,14 @@ public class CalculadoraDeSemelhança {
 	public String relatorio() {
 		
 		String rel = "Relatório de semelhança entre textos: \n" + 
-					 fonte1.toUpperCase() + " x " + fonte2.toUpperCase() + "\n\n" +	
+					 texto1.getFonte().toUpperCase() + " x " + texto2.getFonte().toUpperCase() + "\n\n" +	
 					 "Nível de semelhança = " + semelhanca() + "\n\n" +					 
-					 "- Texto 1 (" + stk1.countTokens() + " palavras) : " + formatTexto1 + " - Fonte: " + fonte1.toUpperCase() + "\n\n" +
-					 "- Texto 2 (" + stk2.countTokens() + " palavras) : " + formatTexto2 + " - Fonte: " + fonte2.toUpperCase() + "\n\n" +					
-					 "- Palavras diferentes do Texto 1 (" + conjTexto1.size() + " palavras) = " + conjTexto1.toString() + "\n\n" +
-					 "- Palavras diferentes do Texto 2 (" + conjTexto2.size() + " palavras) = " + conjTexto2.toString() + "\n\n" +	
+					 "- Texto 1 (" + texto1.getTokenizerSize() + " palavras) : " + texto1.getFormatTexto() + " - Fonte: " + texto1.getFonte().toUpperCase() + "\n\n" +
+					 "- Texto 2 (" + texto2.getTokenizerSize() + " palavras) : " + texto2.getFormatTexto() + " - Fonte: " + texto2.getFonte().toUpperCase() + "\n\n" +					
+					 "- Palavras diferentes do Texto 1 (" + texto1.getConjTextoSize() + " palavras) = " + texto1.getConjTextoString() + "\n\n" +
+					 "- Palavras diferentes do Texto 2 (" + texto2.getConjTextoSize() + " palavras) = " + texto2.getConjTextoString() + "\n\n" +	
 					 "- Palavras diferentes comuns aos dois textos (" + conjTextoComum.size() + " palavras) = " + conjTextoComum.toString() + "\n\n" +
 					 "- Maior trecho comum (" + stkMaxSubseq.countTokens() + " palavras) : \"" + maxSubseq + "\"\n\n";
-					// "Probabilidade da coicidência = 100x[(N - p)^(a - p)]x[(N - a)^(b - p)]/[N^(a + b - p)] = " +
-					//probDeAcaso();
 		return rel;
 	}
-	
-	/*private String probDeAcaso() {
-		DecimalFormat dfmt = new DecimalFormat();
-		dfmt.setMaximumIntegerDigits(3);
-		dfmt.setMinimumIntegerDigits(1);
-		dfmt.setMaximumFractionDigits(2);
-		dfmt.setMinimumFractionDigits(2);
-		
-		double n = totalWords * 2;
-		double a = stk1.countTokens();
-		double b = stk2.countTokens();
-		double p = stkMaxSubseq.countTokens();
-		
-		double prob = (Math.pow(n - p, a - p) * Math.pow(n - a, b - p)) / Math.pow(n, a + b - p) * 100;
-		
-		return dfmt.format(prob) + "%";
-	}	
-	
-	private String tamanho() {
-		if (stk1.countTokens() > stk2.countTokens()) {
-			return "O Texto 1 tem mais palavras que o Texto 2";
-		} else if (stk1.countTokens() < stk2.countTokens()) {
-			return "O Texto 1 tem menos palavras que o Texto 2";
-		} else {
-			return "O Texto 1 e o Texto 2 tem a mesma quantidade de palavras";
-		}
-	}*/
-	
 }
---
+++ Texto.java
@@ -0,0 +1,78 @@
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class Texto {
+	
+	private String fonte; // fonte do texto
+	private String texto; // texto 	
+	private String lowTexto; // texto com todas as letras minúsculas
+	private String formatTexto; // texto com a maior subsequência comum marcada
+	private StringTokenizer stk; // tokenizer do texto
+	private StringTokenizer lowStk; // tokenizer do lowTexto
+	private Set<String> conjTexto; // conjunto de palavras diferentes do texto 
+	
+	public Texto(String fonte, String texto) {
+		// configura as fontes e os textos
+		this.fonte = fonte;
+		this.texto = texto;
+
+		// armazena os textos em letras minúsculas		
+		lowTexto = texto.toLowerCase();
+
+		// separa as palavras dos textos para futura contagem
+		stk = new StringTokenizer(texto, " (){}[]\",.;:");
+
+		// separa as palavras dos textos com minúscula para agrupamento
+		lowStk = new StringTokenizer(lowTexto, " (){}[]\"\',.;:");
+
+		// cria conjuntos para colocar as palavras com letras minúsculas
+		conjTexto = new TreeSet<>();
+
+		// coloca as palavras de cada tokenizer em seus respectivos conjuntos
+		// eliminando assim as duplicatas
+		while (lowStk.hasMoreTokens()) {
+			conjTexto.add(lowStk.nextToken());
+		}
+	}
+
+	// retorna a fonte do texto
+	protected String getFonte() {
+		return fonte;
+	}
+
+	// retorna o corpo do texto
+	protected String getTexto() {
+		return texto;
+	}
+
+	// retorna o corpo do texto com a maior subseqência comum formatada
+	protected String getFormatTexto() {
+		return formatTexto;
+	}
+
+	// formata a maior subequência comum do texto
+	protected void setFormatTexto(String fmtText) {
+		this.formatTexto = fmtText;
+	}
+
+	// retorna a quantidade de palavras do texto separadas pelo StringTokenizer
+	protected int getTokenizerSize() {
+		return stk.countTokens();
+	}
+
+	// retorna a quantidade de palavras diferentes do texto separadas pelo StringTokenizer
+	public int getConjTextoSize() {
+		return conjTexto.size();
+	}
+	
+	// retorna o conjunto de palavras diferentes do texto separadas pelo StringTokenizer
+	public String getConjTextoString() {
+		return conjTexto.toString();
+	}
+
+	public String getConjTexto(int i) {
+		String[] conjTxt = conjTexto.toArray(new String[conjTexto.size()]);
+		return conjTxt[i];
+	}
+}
